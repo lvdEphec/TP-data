2445b928ce8f7d493ea2595ab9e0a03f4649dfef
lvdEphec <33726310+lvdEphec@users.noreply.github.com>
2018-05-14 15:10:46 +0200

    Update tp10_base.js

--- solutions/tp10-acteursFilms/tp10_base.js
+++ solutions/tp10-acteursFilms/tp10_base.js
@@ -1,41 +1,56 @@
-// examen juin 2015
-// ----------------
-// VOTRE CLASSE / NOM / PRENOM ICI
-// -------------------------------
+"use strict";
 
-var acteurSelectionne = undefined;
+// variable globale pour utilisé l'acteur sélectionné dans différentes fonctions: pas beau, mais simple 
+let acteurSelectionne;
 
 function initPage() {
-  xhrReqJson('getActeurs', makeTbActeurs, 'acteurs');
+	let xhr = new XMLHttpRequest();
+	xhr.open('get', 'getActeurs', true);
+    xhr.onload = function() {
+		creerTable(JSON.parse(this.responseText), 'acteurs');
+	};
+	xhr.send();
 }
 
-function getFilms(act) {
-  xhrReqHtml('getFilmsAct?act=' +  act, 'films', showFilms);
-  acteurSelectionne = act;  
+function creerTable(reponseRequete, idBodyTable ) {
+	let lignes = '';
+	for (let e of reponseRequete) {
+		lignes += '<tr>' +
+			'<td onclick="getFilms(\'' + e.id + '\');">'
+			+ e.identite +
+			'</td>' + 
+			'</tr>' + '\n';
+	}
+	document.getElementById(idBodyTable).innerHTML = lignes;
 }
 
-function getAutres(film) {
-  xhrReqHtml('getActsFilm?film=' + film + '&act=' + acteurSelectionne, 'autres', showAutres);    
+function getFilms(acteur) {
+	document.getElementById('autres').innerHTML = '';
+	document.getElementById('asi').style.display = 'none';
+	
+	acteurSelectionne = acteur; // variable globale utilisée, pas de var ou let ici !
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open('get', 'getFilmsAct?act=' + acteur, true); // attention à la construction d'url
+    xhr.onload = function () {
+			document.getElementById('films').innerHTML = xhr.response;	
+			document.getElementById('art').style.display = 'block';
+	}
+	xhr.send();
+	// attention, ne pas mettre l'afichage ici!
 }
 
-function suite(act) {
-  getFilms(act);
+function getAutres(film) {	
+	let xhr = new XMLHttpRequest();
+	// attention : préparer webservice et procédure avec 2 paramètres, le film et l'acteur qu'on ne veut pas afficher
+	xhr.open('get', 'getActsFilm?film=' + film + '&act=' + acteurSelectionne, true); 
+    xhr.onload = function () {
+			document.getElementById('autres').innerHTML = xhr.response;	
+			document.getElementById('asi').style.display = 'block';
+	}
+	xhr.send();
 }
 
-function makeTbActeurs(reponse, id) {
-  lignes = '';
-  for (i in reponse) {
-    lignes += '<tr>' + '<td onclick="getFilms(\'' + reponse[i].id + '\')";>' + reponse[i].identite + '</td>' + '</tr>';
-  }  
-  setElem(id, lignes);
+function suite(film) {
+	getFilms(film);
 }
-
-function showFilms() {
-  getEl('art').style.display = "block";
-}
-
-function showAutres() {
-  getEl('asi').style.display = "block";
-}
-
-
